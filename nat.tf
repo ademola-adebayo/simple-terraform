9de285290e7b051a98aa269743bcf6839457f160
Demola Adebayo <ademoladebayo@gmail.com>
2021-09-26 14:58:40 +0100

    fixing...

--- nat.tf
+++ nat.tf
@@ -15,7 +15,7 @@ resource "aws_route_table" "dev-private-RT" {
   vpc_id  = aws_vpc.development-vpc.id
   route {
    cidr_block  = "0.0.0.0/0"
-nat_gateway_id  = aws_nat_gateway.dev-NAT-GW
+nat_gateway_id  = aws_nat_gateway.dev-NAT-GW.id
   }
 
   tags = {
